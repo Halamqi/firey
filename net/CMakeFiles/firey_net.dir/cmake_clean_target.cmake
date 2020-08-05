272bf659a70b5446b578158570876c8d744eb737
Halamqi <chendaolin_uestc@163.com>
2020-08-05 16:37:16 +0800

    nothing

--- net/CMakeFiles/firey_net.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "../lib/libfirey_net.a"
-)
--- net/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/cdlsxx/firey/net
-# Build directory: /home/cdlsxx/firey/net
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
